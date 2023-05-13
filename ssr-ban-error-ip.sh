5862573cbfd08161aa83cf6d52cc34bb58ff1b8c
cyhfvg <code.guangzhou.zhang@outlook.com>
2023-05-13 20:25:25 +0800

    refactor(`ssr`): `ssr ban error ip`

--- ssr-ban-error-ip.sh
+++ ssr-ban-error-ip.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -x
+#!/usr/bin/env bash
 
 :<<!
 auth: @cyhfvg https://github.com/cyhfvg
@@ -6,7 +6,7 @@ date: 2022/11/16
 
 require: ssr
 
-This script can ban ddos ipv6 address, then restart ssr server automatically.
+This script can ban error ip address, then restart ssr server automatically.
 
 Some code copy from ssr.sh
 
@@ -16,18 +16,22 @@ You can create a cron job to execute this script.
 
 Append this to /etc/crontab for execute every 5 minutes.
 
-*/5 * * * *   root    /bin/bash /root/.scripts/ssr-ban-ipv6-ddos.sh
+*/5 * * * *   root    /bin/bash /root/.scripts/ssr-ban-error-ip.sh
 
 ===
 
-baned_ip_file is the file where these have been baned ipv6 address save to.
+baned_ip_file is the file where these have been baned ip address save to.
 baned_ip_file should be exist.
 baned_ip_file can be empty.
 
 baned_ip_file example:
 
-::ffff:125.46.32.121
-::ffff:111.63.22.72
+125.46.32.121
+111.63.22.72
+
+whitelist_ip_file example:
+
+112.3.250.232
 
 ===
 
@@ -39,6 +43,7 @@ ssserver.log error log example:
 
 # change me {{{1
 baned_ip_file='/root/.scripts/baned-ip.lst'
+whitelist_ip_file='/root/.scripts/whitelist-ip.lst'
 log_file='/usr/local/shadowsocksr/shadowsocks/ssserver.log'
 #
 
@@ -59,8 +64,14 @@ fi
 
 if [ ! -e ${baned_ip_file} ]
 then
-    exit 1
+    touch ${baned_ip_file}
 fi
+
+if [ ! -e ${whitelist_ip_file} ]
+then
+    touch ${whitelist_ip_file}
+fi
+
 if [ ! -e ${log_file} ]
 then
     exit 1
@@ -72,16 +83,25 @@ check_pid(){
 }
 
 
-for ip in $($CAT $log_file  | $GREP -aiP "\d{2}:\d{2}:\d{2} ERROR" | $GREP -aioP "(?<=from ).+(?=:\d+)" | $SORT | $UNIQ)
+for ip in $($CAT $log_file  | $GREP -aiP "\d{2}:\d{2}:\d{2} ERROR" | $GREP -aioP "(?<=from\s::f{4}:)[0-9.]+(?=:\d+)" | $SORT | $UNIQ)
 do
     if $GREP -q "${ip}" $baned_ip_file
     then
-        # echo "${ip}"
-    touch $0
+        printf "ip %s had been baned\n" "${ip}"
+        touch $0
     else
-        $UFW deny from "${ip}" &>/dev/null
-        echo "${ip}" >> $baned_ip_file
-        do_ban=1
+        if $GREP -q "${ip}" $whitelist_ip_file
+        then
+            printf "ip %s in whitelist\n" "${ip}"
+            continue
+        else
+            if $UFW deny from "${ip}" &>/dev/null
+            then
+                echo "${ip}" >> $baned_ip_file
+                do_ban=1
+                printf "ip %s now is be baned\n" "${ip}"
+            fi
+        fi
     fi
 done
 
